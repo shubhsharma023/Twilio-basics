fe4d2389b6666d3c593be3908e6c5ac2e64ad78d
Shubh Sharma <97976855+shubhsharma023@users.noreply.github.com>
2022-12-31 19:05:33 +0530

    removed credentials

--- main.cs
+++ main.cs
@@ -8,12 +8,12 @@ class Program
     static void Main(string[] args)
     {
         // Find your Account Sid and Auth Token at twilio.com/console
-        const string accountSid = "AC72be60b8db893d0ac1822c167e77da4a";
-        const string authToken = "3870557a8a6fcdbf9e0b72e7bb37aa04";
+        const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const string authToken = "your_auth_token";
         TwilioClient.Init(accountSid, authToken);
 
-        var to = new PhoneNumber("+917004480112");
-        var from = new PhoneNumber("+13049697369");
+        var to = new PhoneNumber("<number to which you want to call>");
+        var from = new PhoneNumber("<your active twilio number>");
         var call = CallResource.Create(to, from,
             url: new Uri("http://demo.twilio.com/docs/voice.xml"));
 
